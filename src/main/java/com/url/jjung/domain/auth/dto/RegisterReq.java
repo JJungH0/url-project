1085261299878bceaf9bcc53b73640770f0c1036
JJungH0 <junghwan__@naver.com>
2026-07-04 15:58:21 +0900

    [FEAT]: Auth DTO 설계 및 Repo 인터페이스 설계

---
+++ src/main/java/com/url/jjung/domain/auth/dto/RegisterReq.java
@@ -0,0 +1,26 @@
+package com.url.jjung.domain.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class RegisterReq {
+
+    @Email(message = "이메일 형식이 올바르지 않습니다. 예시 (= xxxx@xxxx.xxx")
+    @NotBlank(message = "이메일을 입력해주세요.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 8자 이상이어야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\\\d)(?=.*[@$!%*#?&]).{8,20}$",
+            message = "비밀번호는 영문, 숫자, 특수문자(@$!%*#?&)를 포함하여 8자 이상 20자 이하로 입력해주세요."
+    )
+    private String password;
+
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    @Size(max = 50,message = "닉네임은 50자 이하여야 합니다.")
+    private String nickname;
+}
